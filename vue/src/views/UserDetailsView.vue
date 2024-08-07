dbeb039c0b968741fc378d9e97c24899dc489e36
MaikoDev <phyonaung@ymail.com>
2024-08-07 12:56:18 -0700

    Changed name of RegisterDetail related files to UserDetails

---
+++ vue/src/views/UserDetailsView.vue
@@ -0,0 +1,18 @@
+<template>
+    <user-details/>
+</template>
+
+<script>
+import UserDetails from "../components/UserDetails.vue";
+
+export default{
+    components:{
+        UserDetails
+    }
+}
+</script>
+
+
+<style>
+
+</style>
