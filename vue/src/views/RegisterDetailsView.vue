944abb8d6c0ed64df793f66f27bc9f98c23efc8d
Andrew Le <andrew.le.careers@gmail.com>
2024-08-07 11:42:50 -0500

    Initialize new view page for registerDetails to utilize the registrationDetails component

---
+++ vue/src/views/RegisterDetailsView.vue
@@ -0,0 +1,18 @@
+<template>
+    <RegistrationDetails/>
+</template>
+
+<script>
+import RegistrationDetails from "../components/RegistrationDetails.vue";
+
+export default{
+    components:{
+        RegistrationDetails
+    }
+}
+</script>
+
+
+<style>
+
+</style>
