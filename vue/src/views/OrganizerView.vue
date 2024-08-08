2042768e273a68af81954f49f390c7dc421c2edf
jenny rodriguez <rjenni002@gmail.com>
2024-08-08 11:44:35 -0500

    created an organizer dashboard

--- vue/src/views/OrganizerView.vue
+++ vue/src/views/OrganizerView.vue
@@ -1,21 +1,82 @@
 <template>
-    <div class="navigation">
-        <router-link to="/organizer/create-tournament">Create Tournament</router-link>
-        
-        
-    </div>
-    <div class="organizerDashboard">
-        <h1>
-            Organizer Dashboard
-        </h1>
-
+    <div class="dashboard-container">
+      <aside class="sidebar">
+        <div class="nav-top">
+        <router-link to="/organizer/create-tournament" class="nav-link">Create Tournament</router-link>
+        <router-link to="/organizer/past-tournaments" class="nav-link">Past Tournaments</router-link>
+      </div>
+      <div class="nav-bottom">
+        <router-link to="/player/dashboard" class="nav-link">Switch to Player</router-link>
+        <router-link to="/organizer/profile" class="nav-link">Profile</router-link>
+        <router-link to="/logout" class="nav-link">Logout</router-link>
+      </div>
+      </aside>
+      <main class="dashboard-content">
+        <h1>Organizer Dashboard</h1>
+       
+      </main>
     </div>
     
-
-</template>
+  </template>
 
 <script>
 export default {
 
 }
 </script>
+<style scoped>
+.dashboard-container {
+  display: flex;
+  height: 100vh;
+}
+
+.sidebar {
+  width: 200px;
+  background-color: #2c3e50;
+  color: white;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between; 
+
+}
+.nav-top {
+  display: flex;
+  flex-direction: column;
+}
+.nav-bottom {
+  display: flex;
+  flex-direction: column;
+}
+
+.nav-link {
+  color: white;
+  text-decoration: none;
+  display: block;
+  padding: 10px 0;
+  margin-bottom: 10px;
+  justify-content: center;
+  align-items: center; /* Centers content vertically */
+  padding: 10px;
+  margin-bottom: 10px;
+  transition: background-color 0.3s ease;
+  border: 1px solid #34495e;
+  border-radius: 4px;
+  height: 20px;
+}
+
+.nav-link:hover {
+  background-color: #34495e;
+}
+
+.dashboard-content {
+  flex-grow: 1;
+  padding: 20px;
+  background-color: rgb(219, 236, 235);
+}
+
+h1 {
+  font-size: 40px;
+  margin-bottom: 20px;
+}
+</style>
