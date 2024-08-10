a4325970deabb03ab608510c88e74e3c5899e066
Darcy McCusker <darcy.mccusker@gmail.com>
2024-08-10 13:07:04 -0700

    CreateTournament now uses dynamic list of games

---
+++ vue/src/services/GamesService.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const http = axios.create({
+  baseURL: import.meta.env.VITE_REMOTE_API
+});
+
+export default {   
+     getAllGames() {
+         return http.get('/games')
+     }
+}
+
