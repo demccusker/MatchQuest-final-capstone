6f39d649cb82c2c4e06aadf3aa5f8bac03f2ff06
Phyo Naung <phyonaung32@yahoo.com>
2024-08-15 06:40:03 -0700

    Mock Data change

--- java/database/data_debug.sql
+++ java/database/data_debug.sql
@@ -119,10 +119,10 @@ INSERT INTO bracket (bracket_id, parent_bracket, match_id, name) VALUES
 --
 ---- Inserting into Tournament
 INSERT INTO tournament (game_id, bracket_id, creator_id, name, max_participants, is_scrim, is_online, location, start_date, end_date) VALUES
-    ((SELECT game_id FROM game WHERE name = 'Tennis'), 510, 1, 'Epic Game of Tennis', 30, FALSE, FALSE, '1200 Smith Street', CURRENT_DATE, NULL),
+    ((SELECT game_id FROM game WHERE name = 'Tennis'), null, 1, 'Epic Game of Tennis', 30, FALSE, FALSE, '1200 Smith Street', CURRENT_DATE, NULL),
     ((SELECT game_id FROM game WHERE name = 'Golf'), null, 2, 'Golf Tournament', 20, FALSE, FALSE, '650 Yankee Avenue', CURRENT_DATE, NULL),
     ((SELECT game_id FROM game WHERE name = 'Golf'), null, 2, 'Golfy Tournament', 15, TRUE, TRUE, NULL, CURRENT_DATE, NULL),
-    ((SELECT game_id FROM game WHERE name = 'Minecraft'), null, 3, 'Minecraft Tournament of Champs', 8, FALSE, TRUE, NULL, '2024-01-01', '2024-01-02'),
+    ((SELECT game_id FROM game WHERE name = 'Minecraft'), 510, 3, 'Minecraft Tournament of Champs', 8, FALSE, TRUE, NULL, '2023-01-01', '2023-01-02'),
     ((SELECT game_id FROM game WHERE name = 'Chess'), null, 3, 'Mind Games Tournament', 64, FALSE, TRUE, NULL, '2026-01-01', NULL),
     ((SELECT game_id FROM game WHERE name = 'Pickleball'), null, 3, 'PT pickky', 64, FALSE, FALSE, NULL, '2024-08-16', NULL),
     ((SELECT game_id FROM game WHERE name = 'Pool'), null, 3, 'Pooly', 64, FALSE, FALSE, NULL, '2026-08-16', NULL),
